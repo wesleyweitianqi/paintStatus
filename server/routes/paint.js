3cdf4e979e401b3207d0d82b3c1c94830104127e
wesleyweitianqi <wesleytianqiwei@gmail.com>
2024-01-21 21:41:23 -0500

    fix refresh bug

--- server/routes/paint.js
+++ server/routes/paint.js
@@ -1,42 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Status = require('../models/paintStatus');
+const Status = require("../models/paintStatus");
 
-router.get('/', async (req, res, next) => {
-    try{
-      const paintedList = await Status.find();
-      res.send({code:0, data: paintedList});
-    }catch(e){
-      console.log(e);
-    }
-  });
-  
-  router.post("/", async (req, res, next) => {
-    try{
-      const { wo, isPainted } = req.body;
-      const paintedWo =await new Status({
-        wo: wo,
-        painted: isPainted
-      })
-      paintedWo.save();
-      const list = await Status.find();
-      res.send({code: 0, data: list});
-  
-    }catch(e){
-      console.log(e);
-    }
-  })
+router.get("/", async (req, res, next) => {
+  try {
+    const paintedList = await Status.find();
+    res.send({ code: 0, data: paintedList });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-  router.post("/delete", async (req, res, next)=> {
-    try{
-      const { deleteWo } = req.body;
+router.post("/", async (req, res, next) => {
+  try {
+    const { wo, isPainted } = req.body;
+    const paintedWo = await new Status({
+      wo: wo,
+      painted: isPainted,
+    });
+    await paintedWo.save();
+    const list = await Status.find();
+    res.send({ code: 0, data: list });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-      const list = await Status.findOneAndDelete({wo: deleteWo});
-      res.send({code:0, data: list});
-    }catch(e){
-      console.log(e);
-    }
-    
-  })
+router.post("/delete", async (req, res, next) => {
+  try {
+    const { deleteWo } = req.body;
+
+    const list = await Status.findOneAndDelete({ wo: deleteWo });
+    res.send({ code: 0, data: list });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 module.exports = router;
